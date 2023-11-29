3d5da3368a9a10768dec3c3b78420ac3e337aab8
Manu S <115629276+manumanu2003@users.noreply.github.com>
2023-11-29 02:10:28 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+mongoose.connect('mongodb://127.0.0.1:27017/vcet')
+    .then(() => {
+        console.log('Connected to vcet database');
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+
+const UserSchema = new mongoose.Schema({
+  firstName:{type:String},
+  lastName:{type:String},
+  address1:{type:String},
+  email:{type:String},
+  city:{type:String},
+  zip:{type:Number},
+});
+
+
+const User = mongoose.model('details', UserSchema);
+
+app.use(express.json());
+app.use(cors());
+
+
+
+
+app.post('/register', async (req, resp) => {
+    try {
+        const user = new User(req.body);
+        const result = await user.save();
+        const userWithoutPassword = result.toObject();
+        delete userWithoutPassword.password;
+
+        resp.send(userWithoutPassword);
+        console.log(userWithoutPassword);
+    } catch (e) {
+        console.error(e);
+        resp.status(500).send('Something Went Wrong');
+    }
+});
+
+app.listen(port, () => {
+    console.log( `App is listening on port ${port}`);
+});
---
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -0,0 +1,145 @@
+import React, { useState, useEffect } from 'react';
+import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core';
+import { useForm, FormProvider } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+
+import { commerce } from '../../lib/commerce';
+import FormInput from './CustomTextField';
+import axios from 'axios'
+const AddressForm = ({ checkoutToken, test }) => {
+  const [shippingCountries, setShippingCountries] = useState([]);
+  const [shippingCountry, setShippingCountry] = useState('');
+  const [shippingSubdivisions, setShippingSubdivisions] = useState([]);
+  const [shippingSubdivision, setShippingSubdivision] = useState('');
+  const [shippingOptions, setShippingOptions] = useState([]);
+  const [shippingOption, setShippingOption] = useState('');
+
+
+  const [firstName,setFirstName]=useState('');
+  const [lastName,setSecondName]=useState('');
+  const [address1,setAddressName]=useState('');
+  const [email,setEmailName]=useState('');
+
+
+  const [city,setCityName]=useState('');
+  const [zip,setZipName]=useState('');
+
+  
+  const methods = useForm();
+
+  const fetchShippingCountries = async (checkoutTokenId) => {
+    const { countries } = await commerce.services.localeListShippingCountries(checkoutTokenId);
+
+    setShippingCountries(countries);
+    setShippingCountry(Object.keys(countries)[0]);
+  };
+
+  const fetchSubdivisions = async (countryCode) => {
+    const { subdivisions } = await commerce.services.localeListSubdivisions(countryCode);
+
+    setShippingSubdivisions(subdivisions);
+    setShippingSubdivision(Object.keys(subdivisions)[0]);
+  };
+
+  const fetchShippingOptions = async (checkoutTokenId, country, stateProvince = null) => {
+    const options = await commerce.checkout.getShippingOptions(checkoutTokenId, { country, region: stateProvince });
+
+    setShippingOptions(options);
+    setShippingOption(options[0].id);
+  };
+
+  useEffect(() => {
+    fetchShippingCountries(checkoutToken.id);
+  }, []);
+
+  useEffect(() => {
+    if (shippingCountry) fetchSubdivisions(shippingCountry);
+  }, [shippingCountry]);
+
+  useEffect(() => {
+    if (shippingSubdivision) fetchShippingOptions(checkoutToken.id, shippingCountry, shippingSubdivision);
+  }, [shippingSubdivision]);
+
+  const push = () => {
+    axios.post('http://localhost:5000/register', { firstName,lastName,address1,email,city,zip})
+    
+      .then((response) => {
+        const result = response.data;
+        if (result) {
+          alert("Data saved successfully");
+        setFirstName('')
+        }
+      })
+      .catch((error) => {
+        console.error("POST request error:", error);
+        alert("Something went wrong when saving data.");
+      });
+  }
+
+  return (
+    <>
+      <Typography variant="h6" gutterBottom>Shipping address</Typography>
+      <FormProvider {...methods}>
+        <form onSubmit={methods.handleSubmit((data) => test({ ...data, shippingCountry, shippingSubdivision, shippingOption }))}>
+          <Grid container spacing={3}>
+           <span onChange={(e)=>setFirstName(e.target.value)}> 
+           <FormInput required name="firstName" label="First name"  /> 
+           </span>
+           <span onChange={(e)=>setSecondName(e.target.value)} >   
+           <FormInput required name="lastName" label="Last name" />
+           </span>
+           <span onChange={(e)=>setAddressName(e.target.value)}>
+           <FormInput required name="address1" label="Address line 1" />
+           </span>
+           <span onChange={(e)=>setEmailName(e.target.value)}> 
+           <FormInput required name="email" label="Email" />
+           </span><span onChange={(e)=>setCityName(e.target.value)}> 
+           <FormInput required name="city" label="City" />
+           </span> 
+           <span onChange={(e)=>setZipName(e.target.value)}> 
+           <FormInput required name="zip" label="Zip / Postal code" />
+           </span>
+           
+            <Grid item xs={12} sm={6}>
+              <InputLabel>Shipping Country</InputLabel>
+              <Select value={shippingCountry} fullWidth onChange={(e) => setShippingCountry(e.target.value)}>
+                {Object.entries(shippingCountries).map(([code, name]) => ({ id: code, label: name })).map((item) => (
+                  <MenuItem key={item.id} value={item.id}>
+                    {item.label}
+                  </MenuItem>
+                ))}
+              </Select>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <InputLabel>Shipping Subdivision</InputLabel>
+              <Select value={shippingSubdivision} fullWidth onChange={(e) => setShippingSubdivision(e.target.value)}>
+                {Object.entries(shippingSubdivisions).map(([code, name]) => ({ id: code, label: name })).map((item) => (
+                  <MenuItem key={item.id} value={item.id}>
+                    {item.label}
+                  </MenuItem>
+                ))}
+              </Select>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <InputLabel>Shipping Options</InputLabel>
+              <Select value={shippingOption} fullWidth onChange={(e) => setShippingOption(e.target.value)}>
+                {shippingOptions.map((sO) => ({ id: sO.id, label: `${sO.description} - (${sO.price.formatted_with_symbol})` })).map((item) => (
+                  <MenuItem key={item.id} value={item.id}>
+                    {item.label}
+                  </MenuItem>
+                ))}
+              </Select>
+            </Grid>
+          </Grid>
+          <br />
+          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Button component={Link} variant="outlined" to="/cart">Back to Cart</Button>
+            <Button type="submit" variant="contained" color="primary" onClick={push}>Next</Button>
+          </div>
+        </form>
+      </FormProvider>
+    </>
+  );
+};
+
+export default AddressForm;
